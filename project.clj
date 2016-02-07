ec1a04092199c7829e4047b31e34ef32e641d815
Dennis S. Hennen <dennis@28studios.com>
2016-02-06 20:28:26 -0500

    Update project for coljure 1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject traffic "1.0.0-SNAPSHOT"
   :description "Traffic Light Simulator"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 ; [org.clojure/clojure-contrib "1.7.0"]
+                 ])
